65c2085972031cac91f15d1e4abf791b19170d66
mdantor383800 <mdantor383800@gmail.com>
2025-10-18 12:11:18 +0600

    add margin

--- src/Artoftesting/Navber.jsx
+++ src/Artoftesting/Navber.jsx
@@ -7,10 +7,10 @@ import ShoppingCart from '../component/ShoppingCart'
 export default function Navber() {
 
   return (
-    <div className='bg-[rgb(47,68,104)] py-8 text-white  w-full z-50'>
-      <nav className='container mx-auto flex justify-between'>
+    <div className='bg-[rgb(47,68,104)] py-8 text-white w-full z-50'>
+      <nav className='container mx-5 md:mx-auto flex justify-between'>
         <p className='text-[30px] font-bold'>ArtOfTesting Ecom</p>
-        <div>
+        <div className='mx-9 md:mx-0'>
           <ShoppingCart/>
          </div>
       </nav>
